6deed03f06b44d9d607fad6d2e1e1db374aa8bb6
Josef Gajdusek <atx@atx.name>
2018-07-12 21:51:18 +0200

    examples: run_qemu.sh: Minor cleanup

--- examples/run_qemu.sh
+++ examples/run_qemu.sh
@@ -2,14 +2,19 @@
 
 HOST="127.0.0.1"
 PORT="7766"
+IMAGE="openwrt-x86-64-combined-ext4.img"
+
+if [ ! -f $IMAGE ]; then
+	echo "Image file '$IMAGE' not found"
+	echo "You can download an OpenWRT image on 'https://downloads.openwrt.org/snapshots/targets/x86/64/'"
+	exit 1
+fi
 
 qemu-system-x86_64 \
-	-drive file=openwrt-x86-64-combined-ext4.img,id=d0,if=none -device ide-hd,drive=d0,bus=ide.0 \
-	-drive if=none,id=usbstick,file=/tmp/ii.img \
-	-nographic -m 1024 \
+	-drive file=$IMAGE,id=d0,if=none -device ide-hd,drive=d0,bus=ide.0 \
+	-nographic -m 256 \
 	-usb \
 	-device ich9-usb-uhci1,id=usb \
-	-device usb-storage,bus=usb.0,drive=usbstick \
 	-chardev socket,id=charredir,host="$HOST",port="$PORT",reconnect=2 \
 	-device usb-redir,chardev=charredir,id=redir,bus=usb.0
 
