6478e2c454eafc2ea4a5e2d1b5942aeb8b2baa62
Josef Gajdusek <atx@atx.name>
2018-07-13 17:44:19 +0200

    setup.py: Add

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="fakeusb",
+    version="0.1",
+    author="Josef Gajdusek",
+    author_email="atx@atx.name",
+    license="MIT",
+    packages=find_packages(),
+)
